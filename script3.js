c5cdc064c94d969639d8d1c9784a2744ab7af68d
Alireza Salimian <alirezasalimian1381@yahoo.com>
2025-03-27 13:01:42 +0330

    homeWork1

---
+++ script3.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const inputs = document.querySelectorAll("input");
+    const resultTag = document.getElementById("result");
+
+    function calculate() {
+        const fee = document.getElementById("fee").value.trim() || 0;
+        const count = document.getElementById("count").value.trim();
+        const discount = document.getElementById("discount").value.trim() || 0;
+
+        if (isNaN(fee) || isNaN(count) || isNaN(discount) || count == 0) {
+            resultTag.textContent = "Invalid input! Please enter valid numbers.";
+            resultTag.classList.remove("alert-info");
+            resultTag.classList.add("alert-danger");
+            return;
+        }
+
+        let feeNum = parseFloat(fee);
+        let countNum = parseFloat(count);
+        let discountNum = parseFloat(discount);
+
+        if (discount < 0) {
+            resultTag.textContent = "Invalid input! Please enter valid numbers."
+            resultTag.classList.remove("alert-info");
+            resultTag.classList.add("alert-danger");
+            return;
+        }
+        
+        let result = (feeNum / countNum) + discountNum;
+
+        resultTag.textContent = `Result: ${result}`;
+        resultTag.classList.remove("alert-danger");
+        resultTag.classList.add("alert-info");
+    }
+
+    inputs.forEach(input => input.addEventListener("input", calculate));
+});
